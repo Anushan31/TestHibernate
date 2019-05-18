737d5c1d1463a23bfa79a552f47a9dd1fe0632e6
hpanushan <hpanushan@gmail.com>
2019-05-18 15:08:56 +0530

    no message

--- src/main/java/com/hpanushan/TestHibernate/App.java
+++ src/main/java/com/hpanushan/TestHibernate/App.java
@@ -5,18 +5,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         Employee emp1 = new Employee();
-        emp1.setId(359);
-        emp1.setName("Nimesh");
-        emp1.setSalary(1000);
+        emp1.setId(344);
+        emp1.setName("Austin");
+        emp1.setSalary(600);
         
         Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
         
